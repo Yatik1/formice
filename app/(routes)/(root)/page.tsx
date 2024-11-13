8592134d7c4f22ea0a431d6951a7a5da6af13fcd
Yatik1 <yatiksrivastava1@gmail.com>
2024-11-13 15:57:42 +0530

    home

---
+++ app/(routes)/(root)/page.tsx
@@ -0,0 +1,13 @@
+
+import Navbar from '@/components/Navbar'
+import React from 'react'
+
+function page() {
+  return (
+    <div className='flex flex-col min-h-screen min-w-full max-h-screen'>
+        
+    </div>
+  )
+}
+
+export default page
