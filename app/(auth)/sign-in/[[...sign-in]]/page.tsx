57231ebed1e077d934426830b721ab2ee2bbc516
Yatik1 <yatiksrivastava1@gmail.com>
2024-11-13 15:57:35 +0530

    sign-in route

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -2,6 +2,8 @@ import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
     return(
-        <SignIn />
+        <div className="w-full h-screen flex items-center justify-center">
+            <SignIn />
+        </div>
     )
 }
